73e78a7c5bbe72506a5e09dc3d1fcd23c7a9dfa8
Alena <7182001562alena@gmail.com>
2021-11-23 14:05:29 -0500

    add negative tests for auth sec

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -13,4 +13,23 @@ describe('Auth', function () {
         await expect(ProfilePage.iconUser)
             .toBeDisplayed();
     });
+
+    it('Log in attempt with non-registered email', async function () {
+        await LoginPage.login('testinvalid@gmail.com', 'testinvalid');
+        await expect (LoginPage.notification).toHaveText('Email is not registered');
+    });
+
+    it('Log in attempt with invalid password', async function () {
+        await LoginPage.login('7182001562alena@gmail.com', 'testinvalid');
+        await expect (LoginPage.notification).toHaveText('Incorrect password');
+    });
+
+    it.only('Credentials are required', async function () {
+        await LoginPage.inputUsername.setValue('test');
+        await LoginPage.inputUsername.smartClear();
+        await expect (LoginPage.loginError).toHaveText('Required');
+        await LoginPage.inputPassword.setValue('test');
+        await LoginPage.inputPassword.smartClear();
+        await expect (LoginPage.passwordError).toHaveText('Required');
+    });
 });
